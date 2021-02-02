9b049614bb6cd6d206b20d71a15a3b581a213647
crogers <creedrogers@gmail.com>
2021-02-01 22:25:45 -0500

    movement handled by Level1 and not by the ship

--- Level1.gd
+++ Level1.gd
@@ -1,11 +1,10 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func _physics_process(delta):
+	var speed = 4
+	$Ship.translate(transform.basis.z * delta * speed)
+	
+	print($Ship.rotation)
--- Ship.gd
+++ Ship.gd
@@ -2,54 +2,71 @@ extends KinematicBody
 
 var speed = 4
 
-func get_input(delta):
-	
-	# rotate
-	#
-	
+func get_input(delta):	
 	if Input.is_action_pressed("rotate_x"):
 		# this is mapped to 'q' in the project->project settings->input map
-		print("rotate_x(delta)")
 		rotate_x(delta)
 		
 	if Input.is_action_pressed("rotate_y"):
 		# this is mapped to 'w' in the project->project settings->input map
-		print("rotate_y(delta)")
 		rotate_y(delta)
 		
 	if Input.is_action_pressed("rotate_z"):
 		# this is mapped to 'e' in the project->project settings->input map
-		print("rotate_z(delta)")
 		rotate_z(delta)
 		
 	if Input.is_action_pressed("neg_rotate_x"):
 		# this is mapped to 'a' in the project->project settings->input map
-		print("neg_rotate_x(delta)")
 		rotate_x(-1 * delta)
 		
 	if Input.is_action_pressed("neg_rotate_y"):
 		# this is mapped to 's' in the project->project settings->input map
-		print("neg_rotate_y(delta)")
 		rotate_y(-1 * delta)
 		
 	if Input.is_action_pressed("neg_rotate_z"):
 		# this is mapped to 'd' in the project->project settings->input map
-		print("neg_rotate_z(delta)")
 		rotate_z(-1 * delta)
-	
+
+################################################################################
+
 	if Input.is_action_pressed("ui_left"):
 		rotate_y(delta)
 		
 	if Input.is_action_pressed("ui_right"):
 		rotate_y(-1* delta)
-		
+
 	if Input.is_action_pressed("ui_up"):
 		translate(transform.basis.y * speed * delta)
 		
 	if Input.is_action_pressed("ui_down"):
 		translate(-1 * transform.basis.y * speed * delta)
 		
+################################################################################
+		
+	if Input.is_action_pressed("translate_x"):
+		# this is mapped to 'r' in the project->project settings->input map
+		translate(transform.basis.x * speed * delta)
+		
+	if Input.is_action_pressed("translate_y"):
+		# this is mapped to 't' in the project->project settings->input map
+		translate(transform.basis.y * speed * delta)
+		
+	if Input.is_action_pressed("translate_z"):
+		# this is mapped to 'y' in the project->project settings->input map
+		translate(transform.basis.z * speed * delta)
+
+	if Input.is_action_pressed("neg_translate_x"):
+		# this is mapped to 'f' in the project->project settings->input map
+		translate(-1 * transform.basis.x * speed * delta)
+		
+	if Input.is_action_pressed("neg_translate_y"):
+		# this is mapped to 'g' in the project->project settings->input map
+		translate(-1 * transform.basis.y * speed * delta)
+		
+	if Input.is_action_pressed("neg_translate_z"):
+		# this is mapped to 'h' in the project->project settings->input map
+		translate(-1 * transform.basis.z * speed * delta)
 
 func _physics_process(delta):
 	get_input(delta)
-	translate(transform.basis.z * speed * delta)
+	#translate(transform.basis.z * speed * delta)
