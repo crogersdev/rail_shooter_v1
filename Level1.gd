6678b659eff5eba0950e7ee414e3b59a8275281a
crogers <creedrogers@gmail.com>
2021-02-02 23:12:03 -0500

    fooling around with more movement

--- Level1.gd
+++ Level1.gd
@@ -1,10 +1,25 @@
 extends Spatial
 
+var speed = 4
+
 func _ready():
-	pass # Replace with function body.
-	
+	pass
+
+func get_input(delta):	
+	if Input.is_action_pressed("ui_left"):
+		$Ship.translate(transform.basis.x * speed * delta)
+		
+	if Input.is_action_pressed("ui_right"):
+		$Ship.translate(-1 * transform.basis.x * speed * delta)
+
+	if Input.is_action_pressed("ui_up"):
+		$Ship.translate(transform.basis.y * speed * delta)
+		
+	if Input.is_action_pressed("ui_down"):
+		$Ship.translate(-1 * transform.basis.y * speed * delta)
+
 func _physics_process(delta):
-	var speed = 4
+	get_input(delta)
 	$Ship.translate(transform.basis.z * delta * speed)
 	
 	print($Ship.rotation)
--- Ship.gd
+++ Ship.gd
@@ -3,70 +3,22 @@ extends KinematicBody
 var speed = 4
 
 func get_input(delta):	
-	if Input.is_action_pressed("rotate_x"):
-		# this is mapped to 'q' in the project->project settings->input map
-		rotate_x(delta)
-		
-	if Input.is_action_pressed("rotate_y"):
-		# this is mapped to 'w' in the project->project settings->input map
-		rotate_y(delta)
-		
-	if Input.is_action_pressed("rotate_z"):
-		# this is mapped to 'e' in the project->project settings->input map
-		rotate_z(delta)
-		
-	if Input.is_action_pressed("neg_rotate_x"):
-		# this is mapped to 'a' in the project->project settings->input map
-		rotate_x(-1 * delta)
-		
-	if Input.is_action_pressed("neg_rotate_y"):
-		# this is mapped to 's' in the project->project settings->input map
-		rotate_y(-1 * delta)
-		
-	if Input.is_action_pressed("neg_rotate_z"):
-		# this is mapped to 'd' in the project->project settings->input map
-		rotate_z(-1 * delta)
-
-################################################################################
 
 	if Input.is_action_pressed("ui_left"):
-		rotate_y(delta)
+		rotate_z(delta * -.5)
 		
 	if Input.is_action_pressed("ui_right"):
-		rotate_y(-1* delta)
+		rotate_z(delta * .5)
 
 	if Input.is_action_pressed("ui_up"):
-		translate(transform.basis.y * speed * delta)
+		rotate_x(delta * -.75)
 		
 	if Input.is_action_pressed("ui_down"):
-		translate(-1 * transform.basis.y * speed * delta)
-		
-################################################################################
-		
-	if Input.is_action_pressed("translate_x"):
-		# this is mapped to 'r' in the project->project settings->input map
-		translate(transform.basis.x * speed * delta)
-		
-	if Input.is_action_pressed("translate_y"):
-		# this is mapped to 't' in the project->project settings->input map
-		translate(transform.basis.y * speed * delta)
-		
-	if Input.is_action_pressed("translate_z"):
-		# this is mapped to 'y' in the project->project settings->input map
-		translate(transform.basis.z * speed * delta)
-
-	if Input.is_action_pressed("neg_translate_x"):
-		# this is mapped to 'f' in the project->project settings->input map
-		translate(-1 * transform.basis.x * speed * delta)
-		
-	if Input.is_action_pressed("neg_translate_y"):
-		# this is mapped to 'g' in the project->project settings->input map
-		translate(-1 * transform.basis.y * speed * delta)
-		
-	if Input.is_action_pressed("neg_translate_z"):
-		# this is mapped to 'h' in the project->project settings->input map
-		translate(-1 * transform.basis.z * speed * delta)
+		rotate_x(delta * .75)
 
 func _physics_process(delta):
 	get_input(delta)
+	
+	# if translate stays in here, we move in the +z direction irrespective
+	# of ship pitch, yaw, and roll
 	#translate(transform.basis.z * speed * delta)
