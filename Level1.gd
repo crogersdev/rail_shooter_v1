3eac78ce8a77042bd53393b0162d39ed6224a7bf
crogers <creedrogers@gmail.com>
2021-02-07 16:53:12 -0500

    incremental

--- Level1.gd
+++ Level1.gd
@@ -1,25 +1,49 @@
 extends Spatial
 
-var speed = 4
+onready var ship = $Path/Dolly/Ship
+onready var dolly = $Path/Dolly
+onready var camera = $Path/Dolly/Camera
 
-func _ready():
-	pass
+var dolly_speed = 1
 
-func get_input(delta):	
-	if Input.is_action_pressed("ui_left"):
-		$Ship.translate(transform.basis.x * speed * delta)
-		
-	if Input.is_action_pressed("ui_right"):
-		$Ship.translate(-1 * transform.basis.x * speed * delta)
+var strafe_speed = 3
+var turning_left = false
+var turning_right = false
 
-	if Input.is_action_pressed("ui_up"):
-		$Ship.translate(transform.basis.y * speed * delta)
-		
-	if Input.is_action_pressed("ui_down"):
-		$Ship.translate(-1 * transform.basis.y * speed * delta)
+var z_rotation = 0.0
+var max_z_rotation = 99.0
+var barrel_roll = false
+var barrel_roll_finished = 0.0
 
 func _physics_process(delta):
+	dolly.offset += delta * dolly_speed
 	get_input(delta)
-	$Ship.translate(transform.basis.z * delta * speed)
+	#move_camera()
 	
-	print($Ship.rotation)
+func _process(delta):
+	var fps = Engine.get_frames_per_second()
+	# var lerp_interval = 
+	
+func get_input(delta):
+	if Input.is_action_pressed("ui_left"):
+		ship.transform.origin.x += delta * strafe_speed
+		
+	if Input.is_action_pressed("ui_right"):
+		ship.transform.origin.x -= delta * strafe_speed
+
+	if Input.is_action_pressed("ui_up"):
+		ship.translate(ship.transform.basis.y * delta)
+		
+	if Input.is_action_pressed("ui_down"):
+		ship.translate(-1 * ship.transform.basis.y * delta)
+
+func move_camera():
+	var orig = lerp(
+		camera.transform.origin,
+		ship.transform.origin + Vector3(0, 1, 5),
+		0.04
+	)
+	orig.x = clamp(orig.x, -50.0, 50.0)
+	orig.y = clamp(orig.y, -30.0, 30.0)
+	
+	camera.transform.origin = orig
--- Ship.gd
+++ Ship.gd
@@ -1,20 +1,26 @@
 extends KinematicBody
 
 var speed = 4
+var x_rotation_dampener = 10.5
+var y_rotation_dampener = 10.5
+var z_rotation_dampener = 10.5
+
+func reset_ship_rotation(delta):
+	pass
 
 func get_input(delta):	
 
 	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta * -.5)
+		rotate_z(-1 * delta * z_rotation_dampener)
 		
 	if Input.is_action_pressed("ui_right"):
-		rotate_z(delta * .5)
+		rotate_z(delta * z_rotation_dampener)
 
 	if Input.is_action_pressed("ui_up"):
-		rotate_x(delta * -.75)
+		rotate_x(-1 * delta * y_rotation_dampener)
 		
 	if Input.is_action_pressed("ui_down"):
-		rotate_x(delta * .75)
+		rotate_x(delta * y_rotation_dampener)
 
 func _physics_process(delta):
 	get_input(delta)
