b3c9d79f3770acffb37015378be5cb8d669b97c6
crogers <creedrogers@gmail.com>
2021-02-07 20:02:32 -0500

    wip

--- Ship2.gd
+++ Ship2.gd
@@ -1,29 +1,37 @@
 extends KinematicBody
 
 var speed = 4
-var rotation_speed = 1.5
-
+var z_rotation_speed = 2.5
+var x_rotation_speed = 5
 
 func reset_ship_rotation(delta):
 	pass
 
-func get_input(delta):	
-	var rotation = get_rotation()
+func get_input(delta):
+	rotation = get_rotation()
 
-	if Input.is_action_pressed("ui_left"):
-		rotation.z = clamp(rotation.z + speed, 0.2, -0.2)
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(delta * z_rotation_speed)
+		rotation = get_rotation()
+		rotation.z = clamp(rotation.z, -.25, .25)
 		set_rotation(rotation)
 		
-	if Input.is_action_pressed("ui_right"):
-		rotation.z = clamp(rotation.z + speed, -0.2, 0.2)
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(-delta * z_rotation_speed)
+		rotation = get_rotation()
+		rotation.z = clamp(rotation.z, -.25, .25)
 		set_rotation(rotation)
 
 	if Input.is_action_pressed("ui_up"):
-		rotation.x = clamp(rotation.x + speed, 0.2, -0.2)
+		rotate_x(-delta * x_rotation_speed)
+		rotation = get_rotation()
+		rotation.x = clamp(rotation.x, -.5, .5)
 		set_rotation(rotation)
 		
 	if Input.is_action_pressed("ui_down"):
-		rotation.x = clamp(rotation.x + speed, -0.2, 0.2)
+		rotate_x(delta * x_rotation_speed)
+		rotation = get_rotation()
+		rotation.x = clamp(rotation.x, -.5, .5)
 		set_rotation(rotation)
 
 func _physics_process(delta):
