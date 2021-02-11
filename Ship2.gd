5db1207d4f6320d7143773f2acd8d9186079bdde
crogers <creedrogers@gmail.com>
2021-02-10 21:19:26 -0500

    wip

--- Ship2.gd
+++ Ship2.gd
@@ -1,8 +1,9 @@
 extends KinematicBody
 
 var speed = 4
-var z_rotation_speed = 2.5
 var x_rotation_speed = 5
+var y_rotation_speed = 2.5
+var z_rotation_speed = 2.5
 
 func reset_ship_rotation(delta):
 	pass
@@ -15,11 +16,23 @@ func get_input(delta):
 		rotation = get_rotation()
 		rotation.z = clamp(rotation.z, -.25, .25)
 		set_rotation(rotation)
+		return
+		
+		rotate_y(-delta * y_rotation_speed)
+		rotation = get_rotation()
+		rotation.y = clamp(rotation.y, -.25, .25)
+		set_rotation(rotation)
 		
 	if Input.is_action_pressed("ui_left"):
 		rotate_z(-delta * z_rotation_speed)
 		rotation = get_rotation()
 		rotation.z = clamp(rotation.z, -.25, .25)
+		set_rotation(rotation)
+		return
+		
+		rotate_y(delta * y_rotation_speed)
+		rotation = get_rotation()
+		rotation.y = clamp(rotation.y, -.25, .25)
 		set_rotation(rotation)
 
 	if Input.is_action_pressed("ui_up"):
