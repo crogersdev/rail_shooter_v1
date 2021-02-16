b173dda91f1e6a0e1ce591ece912433122a29cc4
Chris Rogers <creedrogers@gmail.com>
2021-02-16 00:28:05 -0500

    reimport the z-axis rectified model for the plane so it doesn't keep turning around; changing + to a - in the script

--- Level1.gd
+++ Level1.gd
@@ -6,7 +6,7 @@ onready var dolly = $Path/Dolly
 onready var camera = $Path/Dolly/Camera
 
 var dolly_speed = 5
-var strafe_speed = 100
+var strafe_speed = 20
 
 var turning_left = false
 var turning_right = false
@@ -26,44 +26,54 @@ func get_input(delta):
 		target.transform.origin.y -= strafe_speed * delta
 
 	if Input.is_action_pressed("ui_right"):
-		target.transform.origin.x -= delta * strafe_speed
+		target.transform.origin.x += delta * strafe_speed
 		turning_right = true
 	else:
 		turning_right = false
 
 	if Input.is_action_pressed("ui_left"):
-		target.transform.origin.x += delta * strafe_speed
+		target.transform.origin.x -= delta * strafe_speed
 		turning_left = true
 	else:
 		turning_left = false
-	
+		
 	# straighten us out
 	if !turning_left and !turning_right:
 		target.transform.origin.x = lerp(target.transform.origin.x, ship.transform.origin.x, 0.05)
 		target.transform.origin.y = lerp(target.transform.origin.y, ship.transform.origin.y, 0.05)
 
-	target.transform.origin.y = clamp(target.transform.origin.y, -7.0, 7.0)
-	target.transform.origin.x = clamp(target.transform.origin.x, -10.0, 10.0)
+	target.transform.origin.y = clamp(target.transform.origin.y, 0, 6.0)
+	target.transform.origin.x = clamp(target.transform.origin.x, -7.0, 7.0)
 
 func move_ship(delta):
+	
+
+
 	var desired_rotation = ship.transform.looking_at(target.transform.origin, Vector3(0, 1, 0))
-	var rotation = Quat(ship.transform.basis.get_rotation_quat()).slerp(desired_rotation.basis.get_rotation_quat(), 0.2)
+	var rotation = Quat(
+		ship.transform.basis.get_rotation_quat()
+	).slerp(
+		desired_rotation.basis.get_rotation_quat(),
+		0.02
+	)
 
 	#                                 LR = left/right, UD = up/down, FB = front/back
 	#                                                 LR  UD  FB
-	#var velocity = (target.transform.origin + Vector3(00, 00, dolly_speed)) + ship.transform.origin
-	#velocity = ship.move_and_slide(velocity, Vector3(0, 1, 0))
+	var velocity = (
+		target.transform.origin + Vector3(00, 00, dolly_speed)
+	) - ship.transform.origin
+
+	velocity = ship.move_and_slide(velocity, Vector3(0, 1, 0))
 	ship.transform.origin.y = clamp(ship.transform.origin.y, -7.0, 7.0)
 	ship.transform.origin.x = clamp(ship.transform.origin.x, -10.0, 10.0)
-	print(ship.transform)
-	#ship.set_transform(Transform(rotation, ship.transform.origin))
+	ship.set_transform(Transform(rotation, ship.transform.origin))
 
 func move_camera():
 	# move the camera via lerp to ship position + vector3, where vector3 represents
 	# an offset.  moving 'forward' in the level is a +Z movement, so we want to stay
 	# behind the ship...  which means we stay -5 behind :D
 	# todo: make it configurable and not hardcoded
-	var orig = lerp(camera.transform.origin, ship.transform.origin + Vector3(0, 1, -5), 0.04)
+	var orig = lerp(camera.transform.origin, ship.transform.origin + Vector3(0, 1, 5), 0.04)
 	orig.x = clamp(orig.x, -50.0, 50.0)
 	orig.y = clamp(orig.y, -30.0, 30.0)
 	camera.transform.origin = orig
--- Ship2.gd
+++
@@ -1,7 +0,0 @@
-extends KinematicBody
-
-func get_input(delta):
-	pass
-
-func _physics_process(delta):
-	pass
